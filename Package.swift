3ba12764a61fc0b39eb30cc3a67046c15afb028d
John Pope <jp@fieldstormapp.com>
2017-05-15 22:20:47 -0400

    ⛳️  initial commit

---
+++ Package.swift
@@ -0,0 +1,12 @@
+import PackageDescription
+
+let package = Package(
+    name: "gRPCTensorFlow",
+    targets: [
+        Target(name: "gRPCTensorFlow")
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0,9,902)),
+        .Package(url: "https://github.com/grpc/grpc-swift.git", Version(0,1,10)),
+    ]
+)
