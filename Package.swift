de49fac1caf49bdd51aba453ab04f1e884a3381f
John Pope <jp@bellgeorge.com>
2017-06-07 15:05:14 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
         Target(name: "gRPCTensorFlow")
     ],
     dependencies: [
-        .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0,9,902)),
+        .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0,9,903)),
         .Package(url: "https://github.com/grpc/grpc-swift.git", Version(0,1,10))
     ]
 )
