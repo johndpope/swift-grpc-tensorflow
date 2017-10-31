b5e5346d7ba7de55aa7ca916908939c1e5b3c4a7
John Pope <jp@bellgeorge.com>
2017-10-31 10:58:07 -0400

    updates

--- Sources/tensorflow.grpc.server.pb.swift
+++ Sources/tensorflow.grpc.server.pb.swift
@@ -24,6 +24,7 @@
 import Foundation
 import Dispatch
 import gRPC
+import SwiftProtobuf
 
 /// Type for errors thrown from generated server code.
 internal enum Tensorflow_Grpc_WorkerServiceServerError : Error {
--- Sources/tensorflow.serving.server.pb.swift
+++ Sources/tensorflow.serving.server.pb.swift
@@ -24,6 +24,7 @@
 import Foundation
 import Dispatch
 import gRPC
+import SwiftProtobuf
 
 /// Type for errors thrown from generated server code.
 internal enum Tensorflow_Serving_PredictionServiceServerError : Error {
